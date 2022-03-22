ecf995754d6b4af5e976ff1559af16735020e48a
Pam18 <yha.lima@outlook.com>
2022-03-22 09:21:27 -0300

    feat: Inicialização do projeto

---
+++ tracking/src/main/java/br/com/foodtrack/tracking/TrackingApplication.java
@@ -0,0 +1,13 @@
+package br.com.foodtrack.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingApplication.class, args);
+	}
+
+}
---
+++ tracking/src/test/java/br/com/foodtrack/tracking/TrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foodtrack.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
