f44259ff01147dd2eec7324d55cc6a9333ce2dc2
Guilherme Felipe Campos <gui_fc@hotmail.com>
2022-03-23 12:39:43 -0300

    Criacao das Classes/package model

---
+++ tracking/src/main/java/br/com/foodtrack/tracking/model/Cliente.java
@@ -0,0 +1,60 @@
+package br.com.foodtrack.tracking.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer codigoCliente;
+	@Column(name = "nome", length = 50, nullable = true)
+	private String nome;
+	
+	@Column(name = "latitude", nullable = false)
+	private Double latitude;
+	
+	@Column(name = "longitude", nullable = false)
+	private Double longitude;
+
+	public Integer getCodigoCliente() {
+		return codigoCliente;
+	}
+
+	public void setCodigoCliente(Integer codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+}
---
+++ tracking/src/main/java/br/com/foodtrack/tracking/model/Tracking.java
@@ -0,0 +1,108 @@
+package br.com.foodtrack.tracking.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tracking_history")
+public class Tracking {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer codigoRastreamento;
+	
+	@Column(name = "latitude")
+	private Double latitude;
+	
+	@Column(name = "longitude")
+	private Double longitude;
+	
+	@Column(name = "tempo")
+	private LocalDateTime tempo;
+	
+	@Column(name = "statusEntrega", length = 30, nullable = false)
+	private String statusEntrega;
+	
+	@ManyToOne
+	@JoinColumn(name = "idPedido")
+	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn(name = "idEntregador")
+	@JsonIgnoreProperties("listaRastreamentos")
+	private Entregador entregador;
+
+	
+	
+	
+	
+	public Integer getCodigoRastreamento() {
+		return codigoRastreamento;
+	}
+
+	public void setCodigoRastreamento(Integer codigoRastreamento) {
+		this.codigoRastreamento = codigoRastreamento;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public LocalDateTime getTempo() {
+		return tempo;
+	}
+
+	public void setTempo(LocalDateTime tempo) {
+		this.tempo = tempo;
+	}
+
+	public String getStatusEntrega() {
+		return statusEntrega;
+	}
+
+	public void setStatusEntrega(String statusEntrega) {
+		this.statusEntrega = statusEntrega;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Entregador getEntregador() {
+		return entregador;
+	}
+
+	public void setEntregador(Entregador entregador) {
+		this.entregador = entregador;
+	}
+	
+	
+	
+}
