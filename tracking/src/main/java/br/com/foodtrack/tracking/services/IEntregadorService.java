d817ae6b13714283de1e886f1b1a110d99aa2af1
Guilherme Felipe Campos <gui_fc@hotmail.com>
2022-03-24 14:24:32 -0300

    Criando package DTO, service, e a rota Entregadores

---
+++ tracking/src/main/java/br/com/foodtrack/tracking/services/IEntregadorService.java
@@ -0,0 +1,10 @@
+package br.com.foodtrack.tracking.services;
+
+import java.util.List;
+
+import br.com.foodtrack.tracking.DTO.EntregadorDTO;
+
+public interface IEntregadorService {
+
+	public List<EntregadorDTO> buscaTodos();
+}
