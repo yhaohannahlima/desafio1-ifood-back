c3bee1af0221329e725888696ad342af48a85266
padilha27 <bianca10004@live.com>
2022-03-24 15:56:26 -0300

    Adicionando o token

--- tracking/src/main/java/br/com/foodtrack/tracking/Dao/EntregadorDao.java
+++ tracking/src/main/java/br/com/foodtrack/tracking/Dao/EntregadorDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.foodtrack.tracking.model.Entregador;
 
 public interface EntregadorDao extends CrudRepository<Entregador, Integer> {
-
+      public Entregador findByEmailOrSenha(String email, String senha);
 }
