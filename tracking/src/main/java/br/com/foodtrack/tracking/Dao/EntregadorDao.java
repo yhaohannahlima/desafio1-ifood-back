fe3890d60294bf6c06652e616e7802ff591b4505
Guilherme Felipe Campos <gui_fc@hotmail.com>
2022-03-24 09:40:42 -0300

    Criacao do package Dao, com as interfaces

---
+++ tracking/src/main/java/br/com/foodtrack/tracking/Dao/EntregadorDao.java
@@ -0,0 +1,9 @@
+package br.com.foodtrack.tracking.Dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.foodtrack.tracking.model.Entregador;
+
+public interface EntregadorDao extends CrudRepository<Entregador, Integer> {
+
+}
