35729a243f899f42c7a999fbaedec95c87653a16
padilha27 <bianca10004@live.com>
2022-03-25 09:10:29 -0300

    backup

--- tracking/src/main/java/br/com/foodtrack/tracking/controller/EntregadorController.java
+++ tracking/src/main/java/br/com/foodtrack/tracking/controller/EntregadorController.java
@@ -1,30 +1,30 @@
+
+   
 package br.com.foodtrack.tracking.controller;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.foodtrack.tracking.model.Entregador;
+
 import br.com.foodtrack.tracking.services.IEntregadorService;
 
 @RestController
 public class EntregadorController {
 	
-
 	@Autowired
 	private IEntregadorService service;
 	
+	
 	@GetMapping("/entregadores")
-	public ResponseEntity<?> cadastrarNovoEntregador(@RequestBody Entregador novo){
-		   Entregador res = service.criarEntregador(novo);
-		   
-		   if  (res != null) {
-			   return ResponseEntity.ok(res);
-		   }
-		   return ResponseEntity.badRequest().body( "Dados Invalidos");
-}
+	public ResponseEntity<List<Entregador>> buscarTodos(){
+		return ResponseEntity.ok(service.buscaTodos());
+	}
+
 }
 
 
