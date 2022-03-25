544f2c00a1918d3d9e60bf95e61fa126862af00f
padilha27 <bianca10004@live.com>
2022-03-25 15:15:14 -0300

    removendo tokenUtil

--- tracking/src/main/java/br/com/foodtrack/tracking/services/IEntregadorLoginService.java
+++ tracking/src/main/java/br/com/foodtrack/tracking/services/IEntregadorLoginService.java
@@ -8,7 +8,7 @@ import br.com.foodtrack.tracking.security.Token;
 
 public interface IEntregadorLoginService {
 	public Token gerarAcessoEntregador(EntregadorLoginDTO dadosLogin);
-	public Entregador criarUsuario(Entregador novo);
+	public Entregador criarEntregador(Entregador novo);
 	public Entregador alterarDados(Entregador dados);
 	
 	public List<Entregador> recuperarTodos();
