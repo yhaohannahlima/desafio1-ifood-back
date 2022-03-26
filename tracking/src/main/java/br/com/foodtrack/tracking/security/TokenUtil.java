d87f20285424a28bc4ab8b3fd8168e2386abfa0f
padilha27 <bianca10004@live.com>
2022-03-26 18:12:44 -0300

    Finalizando TokenUtil

--- tracking/src/main/java/br/com/foodtrack/tracking/security/TokenUtil.java
+++ tracking/src/main/java/br/com/foodtrack/tracking/security/TokenUtil.java
@@ -1,10 +1,17 @@
 package br.com.foodtrack.tracking.security;
 
-
 import java.security.Key;
+import java.util.Collections;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
 import br.com.foodtrack.tracking.model.Entregador;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -23,47 +30,39 @@ public class TokenUtil {
 		
 		Key secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 		
-		String token = Jwts.builder().setSubject(usuario.getNome(), usuario.getCodigoEntregador())
+		String token = Jwts.builder().setSubject(usuario.getNome())
 								     .setIssuer(EMISSOR)
 								     .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
 								     .signWith(secretKey, SignatureAlgorithm.HS256)
 								     .compact();
-		System.out.println("TOKEN gerado = "+token);
 		return PREFIX + token;
 	}
 	
-	/* posso criar métodos auxiliares para ajudar na validação */
 	private static boolean isExpirationValid(Date expiration) {
-		return expiration.after(new Date(System.currentTimeMillis()));	// a expiração para ser válida tem que ser após a data atual	
+		return expiration.after(new Date(System.currentTimeMillis()));	
 	}
 	private static boolean isEmissorValid(String emissor) {
 		return emissor.equals(EMISSOR);
 	}
-	private static boolean isSubjectValid(String username) {
-		return username!=null && username.length() > 0;
+	private static boolean isSubjectValid(String nome) {
+		return nome !=null && nome.length() > 0;
 	}
 	
 	public static Authentication validate(HttpServletRequest request) {
-		// extrair o token do cabeçalho
 		String token = request.getHeader(HEADER);
-		token = token.replace(PREFIX, ""); // removi o prefixo "Bearer " do token
-		// extrair as infos relevantes que eu quero do token
+		token = token.replace(PREFIX, ""); 
 		
 		Jws<Claims> jwsClaims = Jwts.parserBuilder().setSigningKey(SECRET_KEY.getBytes())
 				                                    .build()
 				                                    .parseClaimsJws(token);
 		
-		String username = jwsClaims.getBody().getSubject();
+		String nome     = jwsClaims.getBody().getSubject();
 		String issuer   = jwsClaims.getBody().getIssuer();
 		Date   expira   = jwsClaims.getBody().getExpiration();
 		
-		// se for válido, retorno um objeto do tipo Authentication
-		if (isSubjectValid(username) && isEmissorValid(issuer) && isExpirationValid(expira)) {
-			// eu preciso criar um objeto de Autenticação
-			// o objeto é bem completo, onde podemos incluir o nível de autorização,
-			// qual a lista de endpoints que o usuário pode acessar e assim por diante... 
-			// nosso caso é mais simples.. pois não estamos tratando endpoints específicos para diferentes usuários
-			return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+		if (isSubjectValid(nome) && isEmissorValid(issuer) && isExpirationValid(expira)) {
+		
+			return new UsernamePasswordAuthenticationToken(nome, null, Collections.emptyList());
 		}
 	
 		return null; 
